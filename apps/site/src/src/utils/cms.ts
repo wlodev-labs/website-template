5d9fd8b06384b18acabbb0b47e4118939426420f
wloodar <wlodardev@gmail.com>
2025-04-29 00:45:41 +0200

    fix: removed dummy src dir inside src dir for astro and bumped astro to v5.7.8

--- apps/site/src/src/utils/cms.ts
+++
@@ -1,6 +0,0 @@
-import { initPayloadClient } from '@wlodev/astro'
-import type { Config } from '../../../cms/src/payload-types'
-
-export const cms = initPayloadClient<Config>({
-    url: `${import.meta.env.CMS_URL}/api`,
-})
