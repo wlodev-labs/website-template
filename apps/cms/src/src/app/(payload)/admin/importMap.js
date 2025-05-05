a6c1e27da918e09103557ed4e14a3730f8fd75b3
wloodar <wlodardev@gmail.com>
2025-05-06 00:40:55 +0200

    fix: removed duplicated src directory in payload

--- apps/cms/src/src/app/(payload)/admin/importMap.js
+++
@@ -1,9 +0,0 @@
-import { Avatar as Avatar_923d0547317b004548abc1cd5a18a061 } from '@wlodev/payload/client'
-import { LogoutButton as LogoutButton_923d0547317b004548abc1cd5a18a061 } from '@wlodev/payload/client'
-import { WlodevClientUploadHandler as WlodevClientUploadHandler_923d0547317b004548abc1cd5a18a061 } from '@wlodev/payload/client'
-
-export const importMap = {
-  "@wlodev/payload/client#Avatar": Avatar_923d0547317b004548abc1cd5a18a061,
-  "@wlodev/payload/client#LogoutButton": LogoutButton_923d0547317b004548abc1cd5a18a061,
-  "@wlodev/payload/client#WlodevClientUploadHandler": WlodevClientUploadHandler_923d0547317b004548abc1cd5a18a061
-}
