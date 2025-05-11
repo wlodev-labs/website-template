673bd3eb75bfda6946267cf47afb4214a94de3e5
wloodar <wlodardev@gmail.com>
2025-05-11 19:51:49 +0200

    feat: initial setup with react, tailwind and sitemap with baseLayout

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config'
 import wlodev from '@wlodev/astro/adapter'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import { general } from './src/config/general'
 
 // https://astro.build/config
 export default defineConfig({
+    ...general,
     adapter: wlodev(),
+    integrations: [react(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 })
---
+++ apps/site/src/config/general.ts
@@ -0,0 +1,7 @@
+import type { AstroConfig } from 'astro'
+
+export const general: Partial<AstroConfig> = {
+    // Production / target url of a website
+    site: 'https://website.wlodev.com',
+    trailingSlash: 'never',
+}
